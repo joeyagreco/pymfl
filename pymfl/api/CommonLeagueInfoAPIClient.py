56f8cb55f6aa82980dc8f47850c94a6af8368234
jg <57115019+joeyagreco@users.noreply.github.com>
2022-08-01 21:09:45 -0500

    added method

--- pymfl/api/CommonLeagueInfoAPIClient.py
+++ pymfl/api/CommonLeagueInfoAPIClient.py
@@ -8,3 +8,9 @@ class CommonLeagueInfoAPIClient(MFLAPIClient):
         url = cls._build_route(cls._MFL_APP_BASE_URL, year, cls._EXPORT_ROUTE)
         url = cls._add_filters(url, ("TYPE", "league"), ("L", league_id), ("JSON", 1))
         return cls._get_for_year_and_league_id(url=url, year=year, league_id=league_id)
+
+    @classmethod
+    def get_rules(cls, *, year: int, league_id: str) -> dict:
+        url = cls._build_route(cls._MFL_APP_BASE_URL, year, cls._EXPORT_ROUTE)
+        url = cls._add_filters(url, ("TYPE", "rules"), ("L", league_id), ("JSON", 1))
+        return cls._get_for_year_and_league_id(url=url, year=year, league_id=league_id)
