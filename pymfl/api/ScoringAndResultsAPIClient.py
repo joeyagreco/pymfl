ea67be67e2f0783abc6284923dac94c9acba8e95
jg <57115019+joeyagreco@users.noreply.github.com>
2022-08-02 20:58:06 -0500

    added method

--- pymfl/api/ScoringAndResultsAPIClient.py
+++ pymfl/api/ScoringAndResultsAPIClient.py
@@ -25,11 +25,31 @@ class ScoringAndResultsAPIClient(MFLAPIClient):
         The week parameter can be "YTD" to give all year-to-date weekly results.
         Private league access restricted to league owners.
         """
-        # 	If the week is specified, it returns the data for that week, otherwise the most current data is returned.
-        # 	If the value is 'YTD', then it returns year-to-date data (or the entire season when called on historical leagues).
+        # If the week is specified, it returns the data for that week, otherwise the most current data is returned.
+        # If the value is 'YTD', then it returns year-to-date data (or the entire season when called on historical leagues).
         week: int = kwargs.pop("week", None)
         filters = [("TYPE", "weeklyResults"), ("L", league_id), ("JSON", 1)]
         cls._add_filter_if_given("W", week, filters)
+        url = cls._build_route(cls._MFL_APP_BASE_URL, year, cls._EXPORT_ROUTE)
+        url = cls._add_filters(url, *filters)
+        return cls._get_for_year_and_league_id(url=url, year=year, league_id=league_id)
+
+    @classmethod
+    def get_live_scoring(cls, *, year: int, league_id: str, **kwargs) -> dict:
+        """
+        Live scoring for a given league and week, including:
+        - Each franchise's current score
+        - How many game seconds remaining that franchise has
+        - Players who have yet to play
+        - Players who are currently playing
+        """
+        # If the week is specified, it returns the data for that week, otherwise the most current data is returned.
+        week: int = kwargs.pop("week", None)
+        # Setting this argument to 1 will return data for non-starters as well
+        details: int = kwargs.pop("details", None)
+        filters = [("TYPE", "liveScoring"), ("L", league_id), ("JSON", 1)]
+        cls._add_filter_if_given("W", week, filters)
+        cls._add_filter_if_given("DETAILS", details, filters)
         url = cls._build_route(cls._MFL_APP_BASE_URL, year, cls._EXPORT_ROUTE)
         url = cls._add_filters(url, *filters)
         return cls._get_for_year_and_league_id(url=url, year=year, league_id=league_id)
