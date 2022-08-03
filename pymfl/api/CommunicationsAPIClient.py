fa29a51521da27760687f57b236179c5e8877610
jg <57115019+joeyagreco@users.noreply.github.com>
2022-08-03 16:02:13 -0500

    added method

--- pymfl/api/CommunicationsAPIClient.py
+++ pymfl/api/CommunicationsAPIClient.py
@@ -17,3 +17,14 @@ class CommunicationsAPIClient(MFLAPIClient):
         url = cls._build_route(cls._MFL_APP_BASE_URL, year, cls._EXPORT_ROUTE)
         url = cls._add_filters(url, *filters)
         return cls._get_for_year_and_league_id(url=url, year=year, league_id=league_id)
+
+    @classmethod
+    def get_message_board_thread(cls, *, year: int, league_id: str, thread_id: str, **kwargs) -> dict:
+        """
+        Display posts in a thread from a league message board.
+        Access restricted to league owners.
+        """
+        filters = [("TYPE", "messageBoardThread"), ("THREAD", thread_id), ("L", league_id), ("JSON", 1)]
+        url = cls._build_route(cls._MFL_APP_BASE_URL, year, cls._EXPORT_ROUTE)
+        url = cls._add_filters(url, *filters)
+        return cls._get_for_year_and_league_id(url=url, year=year, league_id=league_id)
