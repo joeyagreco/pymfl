ff469728e9273fb8554d32b499bd64beeb109ce5
jg <57115019+joeyagreco@users.noreply.github.com>
2022-08-02 21:04:59 -0500

    added method

--- pymfl/api/ScoringAndResultsAPIClient.py
+++ pymfl/api/ScoringAndResultsAPIClient.py
@@ -53,3 +53,38 @@ class ScoringAndResultsAPIClient(MFLAPIClient):
         url = cls._build_route(cls._MFL_APP_BASE_URL, year, cls._EXPORT_ROUTE)
         url = cls._add_filters(url, *filters)
         return cls._get_for_year_and_league_id(url=url, year=year, league_id=league_id)
+
+    @classmethod
+    def get_player_scores(cls, *, year: int, league_id: str, **kwargs) -> dict:
+        """
+        All player scores for a given league/week, including all rostered players as well as all free agents.
+        Private league access restricted to league owners.
+        """
+        # If the week is specified, it returns the data for that week, otherwise the current week data is returned.
+        # If the value is 'YTD', then it returns year-to-date data.
+        # If the value is 'AVG', then it returns a weekly average.
+        week: int | str = kwargs.pop("week", None)
+        # The year for the data to be returned.
+        for_year: int = kwargs.pop("for_year", None)
+        # Pass a list of player ids separated by commas (or just a single player id) to receive back just the info on those players.
+        players: str = kwargs.pop("players", None)
+        # Return only players from this position.
+        position: str = kwargs.pop("position", None)
+        # If set to 'freeagent', returns only players that are fantasy league free agents.
+        status: str = kwargs.pop("status", None)
+        # If set, and a league id passed, it re-calculates the fantasy score for each player according to that league's rules.
+        # This is only valid when specifying the current year and current week.
+        rules: str = kwargs.pop("rules", None)
+        # Limit the result to this many players.
+        count: int = kwargs.pop("count", None)
+        filters = [("TYPE", "playerScores"), ("L", league_id), ("JSON", 1)]
+        cls._add_filter_if_given("W", week, filters)
+        cls._add_filter_if_given("YEAR", for_year, filters)
+        cls._add_filter_if_given("PLAYERS", players, filters)
+        cls._add_filter_if_given("POSITION", position, filters)
+        cls._add_filter_if_given("STATUS", status, filters)
+        cls._add_filter_if_given("RULES", rules, filters)
+        cls._add_filter_if_given("COUNT", count, filters)
+        url = cls._build_route(cls._MFL_APP_BASE_URL, year, cls._EXPORT_ROUTE)
+        url = cls._add_filters(url, *filters)
+        return cls._get_for_year_and_league_id(url=url, year=year, league_id=league_id)
