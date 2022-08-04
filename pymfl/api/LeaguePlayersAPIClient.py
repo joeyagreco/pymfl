8823866050974b5d1cc96f41776649296c157249
jg <57115019+joeyagreco@users.noreply.github.com>
2022-08-04 16:14:58 -0500

    added method

--- pymfl/api/LeaguePlayersAPIClient.py
+++ pymfl/api/LeaguePlayersAPIClient.py
@@ -53,3 +53,14 @@ class LeaguePlayersAPIClient(MFLAPIClient):
         url = cls._build_route(cls._MFL_APP_BASE_URL, year, cls._EXPORT_ROUTE)
         url = cls._add_filters(url, *filters)
         return cls._get_for_year_and_league_id(url=url, year=year, league_id=league_id)
+
+    @classmethod
+    def get_my_watch_list(cls, *, year: int, league_id: str, **kwargs) -> dict:
+        """
+        My Watch List for the current franchise.
+        Access restricted to league owners.
+        """
+        filters = [("TYPE", "myWatchList"), ("L", league_id), ("JSON", 1)]
+        url = cls._build_route(cls._MFL_APP_BASE_URL, year, cls._EXPORT_ROUTE)
+        url = cls._add_filters(url, *filters)
+        return cls._get_for_year_and_league_id(url=url, year=year, league_id=league_id)
