af68091b1d90dbc671f1ae5b53d0b9a7e5ee6e2b
jg <57115019+joeyagreco@users.noreply.github.com>
2022-08-04 16:13:46 -0500

    added method + cleanup

--- pymfl/api/LeaguePlayersAPIClient.py
+++ pymfl/api/LeaguePlayersAPIClient.py
@@ -20,11 +20,33 @@ class LeaguePlayersAPIClient(MFLAPIClient):
         filters = [("TYPE", "playerRosterStatus"), ("L", league_id), ("P", player_id_or_ids), ("JSON", 1)]
         # Week. If a week is specified, it returns the player status for that week.
         # The default is the current Live Scoring week.
-        week: str = kwargs.pop("week", None)
+        week: int = kwargs.pop("week", None)
         # Franchise ID.
         # If present it uses the franchise id to determine which conference or division to use for the purposes of identifying free agents.
         # If not present it uses the user's franchise id.
         # Only matters on deluxe leagues.
+        franchise_id: str = kwargs.pop("franchise_id", None)
+        cls._add_filter_if_given("W", week, filters)
+        cls._add_filter_if_given("F", franchise_id, filters)
+        url = cls._build_route(cls._MFL_APP_BASE_URL, year, cls._EXPORT_ROUTE)
+        url = cls._add_filters(url, *filters)
+        return cls._get_for_year_and_league_id(url=url, year=year, league_id=league_id)
+
+    @classmethod
+    def get_contest_players(cls, *, year: int, league_id: str, **kwargs) -> dict:
+        """
+        On Contest Leagues, this returns all players eligible to be in a franchise's starting lineup.
+        While this request can be used by any league, it's best suited for leagues with the loadRosters setting set to either 'contest' or 'setem'.
+        Access restricted to league owners.
+        """
+        filters = [("TYPE", "contestPlayers"), ("L", league_id), ("JSON", 1)]
+        # Week. If a week is specified, it returns the players for that week.
+        # The default is the upcoming week.
+        week: int = kwargs.pop("week", None)
+        # Franchise ID.
+        # If present it returns the players eligible for that franchise.
+        # Only matters when called by the commissioner.
+        # When called by an owner, this parameter is ignored and the owner's franchise is used.
         franchise_id: str = kwargs.pop("franchise_id", None)
         cls._add_filter_if_given("W", week, filters)
         cls._add_filter_if_given("F", franchise_id, filters)
