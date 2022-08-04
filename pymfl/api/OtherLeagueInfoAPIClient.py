4643ade4ec2f65bfc4d924e2f4159074c9ca9778
jg <57115019+joeyagreco@users.noreply.github.com>
2022-08-04 16:29:01 -0500

    added method

--- pymfl/api/OtherLeagueInfoAPIClient.py
+++ pymfl/api/OtherLeagueInfoAPIClient.py
@@ -69,3 +69,13 @@ class OtherLeagueInfoAPIClient(MFLAPIClient):
         url = cls._build_route(cls._MFL_APP_BASE_URL, year, cls._EXPORT_ROUTE)
         url = cls._add_filters(url, *filters)
         return cls._get_for_year_and_league_id(url=url, year=year, league_id=league_id)
+
+    @classmethod
+    def get_appearance(cls, *, year: int, league_id: str) -> dict:
+        """
+        The skin, home page tabs, and modules within each tab set up by the commissioner for a given league.
+        """
+        filters = [("TYPE", "appearance"), ("L", league_id), ("JSON", 1)]
+        url = cls._build_route(cls._MFL_APP_BASE_URL, year, cls._EXPORT_ROUTE)
+        url = cls._add_filters(url, *filters)
+        return cls._get_for_year_and_league_id(url=url, year=year, league_id=league_id)
