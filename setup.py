35d38cfbc5450279915c72fdbf311b7c59d0ea63
jg <57115019+joeyagreco@users.noreply.github.com>
2022-08-08 13:57:48 -0500

    updated license in setup and version bump

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ with open("pymfl/_version.py") as f:
 with open("README.md") as f:
     read_me = f.read()
 
-with open("LICENSE") as f:
-    license_ = f.read()
-
 setuptools.setup(
     name="pymfl",
     version=pkg_vars["__version__"],
@@ -18,7 +15,7 @@ setuptools.setup(
     description="Python wrapper for the myfantasyleague API.",
     long_description_content_type="text/markdown",
     long_description=read_me,
-    license=license_,
+    license="MIT",
     include_package_data=True,
     packages=setuptools.find_packages(exclude=("test", "docs")),
     install_requires=["setuptools"]
