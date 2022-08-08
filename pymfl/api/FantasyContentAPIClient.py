fb1de94fc4d10ee63e6962c203d443546041aee7
jg <57115019+joeyagreco@users.noreply.github.com>
2022-08-07 22:58:54 -0500

    added method

--- pymfl/api/FantasyContentAPIClient.py
+++ pymfl/api/FantasyContentAPIClient.py
@@ -153,3 +153,23 @@ class FantasyContentAPIClient(MFLAPIClient):
         url = cls._build_route(cls._MFL_APP_BASE_URL, year, cls._EXPORT_ROUTE)
         url = cls._add_filters(url, *filters)
         return cls._get_for_year_and_league_id(url=url, year=year, league_id=league_id)
+
+    @classmethod
+    def get_top_adds(cls, *, year: int, league_id: str, **kwargs) -> dict:
+        """
+        The most added players across all MyFantasyLeague.com-hosted leagues during the current week
+        (or the past 7 days during the pre-season), as well as the percentage of leagues that they've been added in.
+        Only players that have been added in at least 1% of our leagues will be returned.
+        This data would be helpful in creating some sort of "Who's Hot?" list.
+        """
+        filters = [("TYPE", "topAdds"), ("JSON", 1)]
+        # Limits the result to this many players.
+        count: int = kwargs.pop("count", None)
+        # Set this value to FA to only return available free agents.
+        # This option is not available for deluxe leagues.
+        status: str = kwargs.pop("status", None)
+        cls._add_filter_if_given("COUNT", count, filters)
+        cls._add_filter_if_given("STATUS", status, filters)
+        url = cls._build_route(cls._MFL_APP_BASE_URL, year, cls._EXPORT_ROUTE)
+        url = cls._add_filters(url, *filters)
+        return cls._get_for_year_and_league_id(url=url, year=year, league_id=league_id)
