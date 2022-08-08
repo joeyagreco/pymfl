f8b492836920b3618c1b07319c32d95799007314
jg <57115019+joeyagreco@users.noreply.github.com>
2022-08-08 18:30:30 -0500

    fixed bug and version bump

--- pymfl/api/CommonLeagueInfoAPIClient.py
+++ pymfl/api/CommonLeagueInfoAPIClient.py
@@ -102,7 +102,7 @@ class CommonLeagueInfoAPIClient(MFLAPIClient):
         Returns the games (with results if available) of the specified playoff bracket.
         Private league access restricted to league owners.
         """
-        filters = [("TYPE", "schedule"), ("L", league_id), ("JSON", 1), ("BRACKET_ID", bracket_id)]
+        filters = [("TYPE", "playoffBracket"), ("L", league_id), ("JSON", 1), ("BRACKET_ID", bracket_id)]
         url = cls._build_route(cls._MFL_APP_BASE_URL, year, cls._EXPORT_ROUTE)
         url = cls._add_filters(url, *filters)
         return cls._get_for_year_and_league_id(url=url, year=year, league_id=league_id)
