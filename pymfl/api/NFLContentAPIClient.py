a9925c8df8be5a3e0f05867fb35dff67af8f2544
jg <57115019+joeyagreco@users.noreply.github.com>
2022-08-07 23:10:45 -0500

    added method

--- pymfl/api/NFLContentAPIClient.py
+++ pymfl/api/NFLContentAPIClient.py
@@ -18,3 +18,22 @@ class NFLContentAPIClient(MFLAPIClient):
         url = cls._build_route(cls._MFL_APP_BASE_URL, year, cls._EXPORT_ROUTE)
         url = cls._add_filters(url, *filters)
         return cls._get_for_year_and_league_id(url=url, year=year, league_id=league_id)
+
+    @classmethod
+    def get_nfl_schedule(cls, *, year: int, league_id: str, **kwargs) -> dict:
+        """
+        The NFL schedule for one week of the season, including:
+            - The scheduled kickoff of the game
+            - Home team (and their score)
+            - Away team (and their score)
+        Starting with the 2020 season, this request will no longer update while games are in progress, and it's meant to be used to get the NFL schedule.
+        To obtain the current scores of games, please check out this FAQ.
+        """
+        filters = [("TYPE", "nflSchedule"), ("JSON", 1)]
+        # If the week is not specified, it defaults to the current week.
+        # If set to 'ALL', it returns the full season schedule.
+        week: str = kwargs.pop("week", None)
+        cls._add_filter_if_given("W", week, filters)
+        url = cls._build_route(cls._MFL_APP_BASE_URL, year, cls._EXPORT_ROUTE)
+        url = cls._add_filters(url, *filters)
+        return cls._get_for_year_and_league_id(url=url, year=year, league_id=league_id)
