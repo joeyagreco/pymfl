71db7b52908da9e5a1d33e6eac49bf70ac97ca5a
jg <57115019+joeyagreco@users.noreply.github.com>
2022-08-10 21:46:01 -0500

    added dependencies to setup and version bump

--- setup.py
+++ setup.py
@@ -18,5 +18,7 @@ setuptools.setup(
     license="MIT",
     include_package_data=True,
     packages=setuptools.find_packages(exclude=("test", "docs")),
-    install_requires=["setuptools"]
+    install_requires=["setuptools",
+                      "configparser",
+                      "requests"]
 )
